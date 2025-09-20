cc857c22a6573c25aafc1ded128b2ad79ce4e44b
Joshunatoir <joshunatoir2@gmail.com>
2025-09-19 21:16:43 -0600

    this is a first edit of the file which we used for 'learning' input stuffs

---
+++ input name.py
@@ -0,0 +1,13 @@
+#this is a greeting message
+
+print("hello user please input your name for me so I can get to know you better")
+
+name = input("please input your name here:")
+
+print("Hi", name, ", it's a pleasure to meet you!")
+
+age = int(input('What is your age?:'))
+
+birthYear = 2025-age
+
+print("you were born in:", birthYear)
